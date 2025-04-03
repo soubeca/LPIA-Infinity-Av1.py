d72c0dffc7575fcf6fee9e17ea23d4f62b2a354a
Rebeca <84825856+soubeca@users.noreply.github.com>
2025-04-03 03:57:43 +0000

    avaliação aula 02

--- Av2.py
+++ Av2.py
@@ -1,1 +1,15 @@
+#Crie um programa em Python para verificar se um número é positivo, negativo ou zero. 
+# O programa deve solicitar ao usuário que insira um número e, em seguida, imprimir uma mensagem indicando a natureza do número.
+#Se o número for maior que zero, exiba a mensagem "O número é positivo." Se for menor que zero, exiba "O número é negativo."
+#Caso seja zero, a mensagem deve ser "O número é zero."
+#Utilize estruturas condicionais e formatação com F-string para criar o programa de forma clara.
 
+
+number = int(input('Insira um número inteiro: '))
+
+if number > 0:
+    print(f'O número {number} é positivo!')
+elif number < 0:
+    print(f'O número {number} é negativo!')
+else:
+    print(f'O número é {number}.')
